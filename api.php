a768d64ad2b73d53282d63a73c8e4840fb8763b6
shuliman <lastonic.dev@gmail.com>
2023-06-16 16:48:22 +0200

    Added API for frontend

---
+++ api.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once 'CurrencyConverter/CurrencyConverter.php';
+require_once 'CurrencyConverter/Models/ConversionResult.php';
+
+use CurrencyConverter\CurrencyConverter;
+use CurrencyConverter\Models\ConversionResult;
+
+$config = require 'config.php';
+
+$converter = new CurrencyConverter($config);
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && filter_input(INPUT_GET, 'currencies', FILTER_VALIDATE_BOOLEAN)) {
+    $response = [
+        'currencies' => $converter->getCurrencies()
+    ];
+
+    echo json_encode($response);
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //Validate and filter the input data
+    $amount = filter_input(INPUT_POST, 'amount', FILTER_VALIDATE_FLOAT);
+    $sourceCurrency = filter_input(INPUT_POST, 'source_currency', FILTER_SANITIZE_SPECIAL_CHARS);
+    $targetCurrency = filter_input(INPUT_POST, 'target_currency', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if ($amount !== false && $sourceCurrency !== null && $targetCurrency !== null) {
+        //Convert sum
+        $convertedAmount = $converter->convertCurrency($amount, $sourceCurrency, $targetCurrency);
+
+        if ($convertedAmount !== false) {
+            $result = new ConversionResult($amount, $sourceCurrency, $targetCurrency, $convertedAmount, date('Y-m-d H:i:s'));
+
+            //Saving the result of the conversion
+            $converter->saveConversionResult($result);
+        }
+    }
+    //Get list of the latest conversion results
+    $conversionResults = $converter->getConversionResults(5);
+
+    //Return data in JSON format
+    header('Content-Type: application/json');
+
+    //Forming API response
+    $response = [
+        'conversionResults' => $conversionResults
+    ];
+
+    //Output the answer in JSON format
+    echo json_encode($response);
+}
